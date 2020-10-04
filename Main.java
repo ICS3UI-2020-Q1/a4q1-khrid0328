9ffb719f3950c37724a2a5220e5d9817ebefaa70
Adam Lamont <adam_lamont@wrdsb.ca>
2020-10-04 14:28:45 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * The  program will prompt the user to enter in an integer. The program will then start at 100 and count down by 5 until it gets as low as possible without going past the entered number.
+ * @author Dafna Khripun
  */
 public class Main {
 
@@ -9,7 +10,27 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+
+    // Create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // ask the user for the number they want to count down to
+    System.out.println("Please enter an integer to count down to");
+
+    // create a variable for user input
+    int downTo = input.nextInt();
+
+    // create a starting variable to count down from
+    int startingNumber = 100;
+
+    // usign a loop to count down from 100 to users input
+    while(startingNumber >= downTo){
+
+      // print the starting number
+      System.out.println(startingNumber);
+
+      // decrease value of starting number by 5
+      startingNumber = startingNumber - 5;
+    }
   }
 }
